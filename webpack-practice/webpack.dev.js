1ab798bfd69b29b438d69a978bdaaa88001d2f2f
kim jae hun <zg788990@gmail.com>
2020-09-18 14:25:50 +0900

    historyApiFallback & rewrite

--- webpack-practice/webpack.dev.js
+++ webpack-practice/webpack.dev.js
@@ -2,7 +2,18 @@ const {merge} = require('webpack-merge');
 const common  = require('./webpack.common');
 
 const config ={
-  mode:'development'
+  mode:'development',
+  devServer:{
+    open:false,
+    overlay:true,
+    historyApiFallback:{
+      rewrites:[
+        {from:/^\/subpage$/,to:'subpage.html'},
+        {from: /./ ,to:'404.html'}
+      ]
+    },
+    port:3333
+  }
 };
 
 module.exports = merge(common,config);
